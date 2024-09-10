18885e4205081ffc7ab5e8e9416f981b27a6f7e1
Austine Odanga <austinoketches@gmail.com>
2024-09-10 18:07:47 +0300

    feat:  frontend initialization

---
+++ frontend/src/App.jsx
@@ -0,0 +1,26 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Tender from './pages/Tender';
+import Reviews from './pages/Reviews';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tender" element={<Tender />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Tender.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Tender = () => {
+  return (
+    <div>
+      <h1>Tender Page</h1>
+      <p>Here you can find and upload tenders.</p>
+    </div>
+  );
+};
+
+export default Tender;
